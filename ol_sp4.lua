6a7a09103c55a33bf8f40e5b5223ee26638882d6
Ho-spair <linyuy@163.com>
2024-06-12 01:01:49 +0800

    写重复了，就当优化了

--- ol_sp4.lua
+++ ol_sp4.lua
@@ -7,13 +7,19 @@ Fk:loadTranslationTable{
 }
 
 local mawan = General(extension, "mawan", "qun", 4)
+Fk:loadTranslationTable{
+  ["mawan"] = "马玩",
+  -- ["#mawan"] = "",
+  ["~mawan"] = "",
+}
+
+mawan:addSkill("mashu")
+
 local hunjiang = fk.CreateActiveSkill{
   name = "hunjiang",
-  anim_type = "control",
-  card_num = 0,
+  anim_type = "offensive",
   min_target_num = 1,
-  max_target_num = 999,
-  prompt = "#hunjiang-active",
+  card_num = 0,
   can_use = function(self, player)
     return player:usedSkillTimes(self.name, Player.HistoryPhase) == 0
   end,
@@ -23,93 +29,124 @@ local hunjiang = fk.CreateActiveSkill{
   end,
   on_use = function(self, room, effect)
     local player = room:getPlayerById(effect.from)
-    room:sortPlayersByAction(effect.tos)
-    local targets = table.map(effect.tos, function(id) return room:getPlayerById(id) end)
+    local targets = table.simpleClone(effect.tos)
+    room:sortPlayersByAction(targets)
+    targets = table.map(targets, function(pId) return room:getPlayerById(pId) end)
     for _, p in ipairs(targets) do
-      local choices = {"hunjiang1", "hunjiang2"}
-      p.request_data = json.encode({choices, choices, self.name, "#hunjiang-choice:"..player.id})
+      local choices = { "hunjiang_extra_target:" .. player.id, "hunjiang_draw::" .. player.id }
+      p.request_data = json.encode({
+        choices,
+        choices,
+        self.name,
+        "#hunjiang-others_choose",
+      })
     end
-    room:notifyMoveFocus(room.alive_players, self.name)
+    room:notifyMoveFocus(targets, self.name)
     room:doBroadcastRequest("AskForChoice", targets)
 
+    local firstChosen
     for _, p in ipairs(targets) do
-      if not p.reply_ready then
-        p.client_reply = "hunjiang2"
-      end
-    end
-
-    local n = 0
-    for _, p in ipairs(targets) do
-      local choice = p.client_reply
-      if choice == "hunjiang1" then
-        room:setPlayerMark(p, "@@hunjiang-phase", 1)
+      local choice
+      if p.reply_ready then
+        choice = p.client_reply
       else
-        n = n + 1
+        choice = "hunjiang_extra_target:" .. player.id
+      end
+
+      if firstChosen == nil then
+        firstChosen = choice
+      elseif firstChosen ~= choice then
+        firstChosen = false
+      end
+
+      if choice:startsWith("hunjiang_extra_target") then
+        local hunjiangUsers = U.getMark(p, "@@hunjiang-phase")
+        table.insertIfNeed(hunjiangUsers, player.id)
+        room:setPlayerMark(p, "@@hunjiang-phase", hunjiangUsers)
+      else
+        player:drawCards(1, self.name)
       end
     end
-    if n == #targets then
+
+    if firstChosen then
       for _, p in ipairs(targets) do
-        room:setPlayerMark(p, "@@hunjiang-phase", 1)
+        if firstChosen:startsWith("hunjiang_extra_target") then
+          player:drawCards(1, self.name)
+        else
+          local hunjiangUsers = U.getMark(p, "@@hunjiang-phase")
+          table.insertIfNeed(hunjiangUsers, player.id)
+          room:setPlayerMark(p, "@@hunjiang-phase", hunjiangUsers)
+        end
       end
-    end
-    if n > 0 then
-      player:drawCards(n, self.name)
-    else
-      player:drawCards(#targets, self.name)
     end
   end,
 }
-local hunjiang_trigger = fk.CreateTriggerSkill{
-  name = "#hunjiang_trigger",
-  mute = true,
+local hunjiangTarget = fk.CreateTriggerSkill{
+  name = "#hunjiang_target",
   events = {fk.AfterCardTargetDeclared},
+  anim_type = "offensive",
   can_trigger = function(self, event, target, player, data)
-    return target == player and player:usedSkillTimes("hunjiang", Player.HistoryPhase) > 0 and data.card.trueName == "slash" and
-      table.find(player.room:getOtherPlayers(player), function(p)
-        return p:getMark("@@hunjiang-phase") > 0 and table.contains(U.getUseExtraTargets(player.room, data, true), p.id) end)
-  end,
-  on_cost = Util.TrueFunc,
-  on_use = function(self, event, target, player, data)
     local room = player.room
-    local targets = table.map(
-      table.filter(room:getOtherPlayers(player), function(p)
-        return p:getMark("@@hunjiang-phase") > 0 and table.contains(U.getUseExtraTargets(room, data, true), p.id)
-      end),
-    Util.IdMapper)
-    local tos = room:askForChoosePlayers(player, targets, 1, #targets, "#hunjiang-choose:::"..data.card:toLogString(), "hunjiang", true)
-    if #tos > 0 then
-      for _, pid in ipairs(tos) do
-        table.insert(data.tos, {pid})
+    return
+      target == player and
+      data.card.trueName == "slash" and
+      table.find(
+        room.alive_players,
+        function(p)
+          return
+            table.contains(U.getMark(p, "@@hunjiang-phase"), player.id) and
+            not table.contains(TargetGroup:getRealTargets(data.tos), p.id) and
+            U.canUseCardTo(room, player, p, data.card, true)
+        end
+      )
+  end,
+  on_cost = function(self, event, target, player, data)
+    local room = player.room
+    local targets = table.filter(
+      room.alive_players,
+      function(p)
+        return
+          table.contains(U.getMark(p, "@@hunjiang-phase"), player.id) and
+          not table.contains(TargetGroup:getRealTargets(data.tos), p.id) and
+          U.canUseCardTo(room, player, p, data.card, true)
       end
-      room:sendLog{
-        type = "#AddTargetsBySkill",
-        from = player.id,
-        to = tos,
-        arg = "hunjiang",
-        arg2 = data.card:toLogString()
-      }
+    )
+
+    local tos = room:askForChoosePlayers(player, table.map(targets, Util.IdMapper), 1, #targets, "#hunjiang-choose", self.name)
+    if #tos > 0 then
+      self.cost_data = tos
+      return true
     end
+
+    return false
+  end,
+  on_use = function(self, event, target, player, data)
+    for _, pId in ipairs(self.cost_data) do
+      TargetGroup:pushTargets(data.tos, pId)
+    end
+
+    player.room:sendLog{
+      type = "#AddTargetsBySkill",
+      from = player.id,
+      to = self.cost_data,
+      arg = "hunjiang",
+      arg2 = data.card:toLogString()
+    }
   end,
 }
-hunjiang:addRelatedSkill(hunjiang_trigger)
-mawan:addSkill("mashu")
-mawan:addSkill(hunjiang)
 Fk:loadTranslationTable{
-  ["mawan"] = "马玩",
-  --["#mawan"] = "",
-  --["designer:mawan"] = "",
-  --["illustrator:mawan"] = "",
-
   ["hunjiang"] = "浑疆",
-  [":hunjiang"] = "出牌阶段限一次，你可以令攻击范围内任意名其他角色同时选择一项：1.令你本阶段使用【杀】可以指定其为额外目标；2.令你摸一张牌。"..
-  "若这些角色均选择了同一项，也须执行另一项。",
-  ["#hunjiang-active"] = "浑疆：令任意角色选择你可以使用【杀】额外指定其为目标或令你摸牌",
+  [":hunjiang"] = "出牌阶段限一次，你可以令攻击范围内至少一名角色同时选择一项：1.令你于本阶段内使用【杀】可指定其为额外目标；" ..
+  "2.令你摸一张牌。若这些角色均选择同一项，则依次执行另一项。",
+  ["#hunjiang_target"] = "浑疆",
   ["@@hunjiang-phase"] = "浑疆",
-  ["hunjiang1"] = "你成为【杀】的额外目标",
-  ["hunjiang2"] = "其摸一张牌",
-  ["#hunjiang-choice"] = "浑疆：%src 令你选择一项",
-  ["#hunjiang-choose"] = "浑疆：你可以为%arg额外指定任意有“浑疆”标记的角色为目标",
+  ["hunjiang_extra_target"] = "本阶段 %src 使用【杀】可指定你为额外目标",
+  ["hunjiang_draw"] = "令 %dest 摸一张牌",
+  ["#hunjiang-others_choose"] = "浑疆：请选择一项，若你与其他目标均选择同一项，则你执行另一项",
+  ["#hunjiang-choose"] = "浑疆：你可选择其中至少一名角色成为此【杀】的额外目标",
 }
 
+hunjiang:addRelatedSkill(hunjiangTarget)
+mawan:addSkill(hunjiang)
 
 return extension
